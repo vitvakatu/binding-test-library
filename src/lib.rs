0a647c21a1fc499cef06ed1c72c37cc4d512d49f
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-10-28 19:13:47 +0300

    Polish API, impove tests, add documentation and apply rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,56 @@
 extern crate libc;
-use std::slice;
-use std::mem;
 
+use std::{i32, slice};
+
+/// Returns absolute value of a number
 fn abs_internal(i: i32) -> i32 {
     i.abs()
 }
 
+/// Finds maximum value in the slice
+fn max_internal(slice: &[i32]) -> i32 {
+    slice.iter().max().cloned().unwrap_or(i32::MIN)
+}
+
+/// Creates simple vector with desired size filled with `0, 1, 2, ... N-1`
+fn create_vec_internal(size: usize) -> Vec<u8> {
+    (0..size).map(|x| x as u8).collect()
+}
+
+// *************************************************
+//                  Here goes C-API
+// *************************************************
+
 #[no_mangle]
-pub extern fn abs(i: libc::c_int) -> libc::c_int {
+pub extern "C" fn abs(i: libc::c_int) -> libc::c_int {
     abs_internal(i)
 }
 
-fn max_internal(slice: &[i32]) -> i32 {
-    slice.iter().max().cloned().unwrap_or(0)
-}
-
 #[no_mangle]
-pub extern fn max(slice: *const libc::c_int, count: libc::size_t) -> libc::c_int {
-    let slice = unsafe {
-        slice::from_raw_parts(slice, count)
-    };
+pub extern "C" fn max(slice: *const libc::c_int, count: libc::size_t) -> libc::c_int {
+    let slice = unsafe { slice::from_raw_parts(slice, count) };
 
     max_internal(slice)
 }
 
-fn create_vec_internal() -> Vec<u8> {
-    vec![1, 2, 3]
+#[no_mangle]
+pub extern "C" fn fill_vec(buffer: *mut libc::c_uchar, size: libc::size_t) {
+    let vec = create_vec_internal(size as usize);
+
+    let slice = unsafe { slice::from_raw_parts_mut(buffer, size as usize) };
+    slice.copy_from_slice(&vec);
 }
 
 #[no_mangle]
-pub extern fn create_vec() -> *const u8 {
-    let mut vec = create_vec_internal();
-
-    vec.shrink_to_fit();
-    let ptr = vec.as_ptr();
-
-    mem::forget(vec);
-
-    ptr
-}
-
-#[no_mangle]
-pub extern fn use_callback(callback: extern fn(libc::c_int) -> libc::c_int, value: libc::c_int) -> libc::c_int {
+pub extern "C" fn use_callback(
+    callback: extern "C" fn(libc::c_int) -> libc::c_int,
+    value: libc::c_int,
+) -> libc::c_int {
     callback(value)
 }
 
 #[no_mangle]
-pub extern fn return_callback() -> extern fn(libc::c_int) -> libc::c_int {
+pub extern "C" fn return_callback() -> extern "C" fn(libc::c_int) -> libc::c_int {
     abs
 }
 
@@ -67,10 +71,11 @@ mod tests {
         assert_eq!(1, max_internal(&[1]));
         assert_eq!(1, max_internal(&[1, 1]));
         assert_eq!(3, max_internal(&[1, 3, 2]));
+        assert_eq!(5, max_internal(&[1, 2, 5]));
     }
 
     #[test]
     fn create_vec_test() {
-        assert_eq!(vec![1, 2, 3], create_vec_internal());
+        assert_eq!(vec![0, 1, 2], create_vec_internal(3));
     }
 }
